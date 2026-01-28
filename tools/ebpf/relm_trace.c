ea0d3e5d03e85a65558442e183e9472fa91bdf10
Chrinovic Mukanya <chrinovicmu@gmail.com>
2026-01-28 15:33:34 +0200

    eBPF program added

--- tools/ebpf/relm_trace.c
+++ tools/ebpf/relm_trace.c
@@ -34,5 +34,31 @@ int trace_relm_vm_exit(struct relm_vm_exit_args *ctx)
     struct exit_stats new_stats = {}; 
 
     stats = bpf_map_lookup_elem(&exit_stats_map, &exit_reason);
+    if(stats)
+    {
+        __sync_fetch_and_add(&stats->count, 1); 
+        __sync_fetch_and_add(&stats->total_duration, ctx->exit_duration_ns); 
 
+        if(ctx->exit_duration_ns < stats->min_duration || stats->min_duration == 0){
+            stats->min_duration = ctx->exit_duration_ns;
+        }
+
+        if (ctx->exit_duration_ns > stats->max_duration) {
+            stats->max_duration = ctx->exit_duration_ns;
+        }
+    }else{
+        new_stats.count = 1; 
+        new_stats.total_duration = ctx->exit_duration_ns; 
+        new_stats.min_duration = ctx->exit_duration_ns; 
+        new_stats.max_duration = ctx->exit_duration_ns; 
+
+        bpf_map_update_elem(&exit_stats_map, &exit_reason, &new_stats, BPF_ANY); 
+    }
+
+    char fmt[] = "VM exit: vcpu=%u reason=0x%lx duration=%llu ns\n";
+    bpf_trace_printk(fmt, sizeof(fmt), ctx->vpid, exit_reason, ctx->exit_duration_ns);
+
+    return 0;
 }
+
+char _license[] SEC("license") = "GPL"; 
